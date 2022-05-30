23290b4a7f6a660d0979831ea0fd147d1ffabef1
Elias Reyes <elias.rey2526@gmail.com>
2022-05-30 17:43:18 -0500

    paso 2a,b.i - Reyes Sanchez

--- aplicacion.py
+++ aplicacion.py
@@ -5,3 +5,14 @@ Created on Mon May 30 17:29:02 2022
 @author: admin
 """
 
+import gestion_archivos
+
+def menu():
+    print("*MENU PRINCIPAL*")
+    print("1. Crear Archivo")
+    print("2. Eliminar archivo")
+    print("3. Agregar contenido")
+    print("4. Mostrar contenido del archivo")
+    print("5.Salir")
+
+
