4dd815ca5c0cbc4765bb686dc84fe15523fe93b7
Elias Reyes <elias.rey2526@gmail.com>
2022-05-30 16:50:12 -0500

    paso 1.1 - Reyes Sanchez

--- gestion_archivos.py
+++ gestion_archivos.py
@@ -5,3 +5,9 @@ Created on Mon May 30 16:40:36 2022
 @author: admin
 """
 
+import os
+def crear_archivo(nombre,contenido):
+        archivo = open(nombre,"at")
+        archivo.write(contenido)
+        archivo.close()
+
